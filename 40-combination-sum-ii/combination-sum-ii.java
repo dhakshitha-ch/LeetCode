876affa73b97da9acb2389b740ca20702afdd686
Dhakshitha <dhakshithachalla1@gmail.com>
2026-09-13 18:51:56 +0530

    Time: 6 ms (70.12%) | Memory: 45 MB (78.64%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.java
+++ 40-combination-sum-ii/combination-sum-ii.java
@@ -1,40 +1,39 @@
 class Solution {
 
-    static void combsum(int[] candidates, int target,List<List<Integer>> combination , List<Integer> path,int i)
-    {   
-        if(target==0 )
+    static void backtrack(int[] candidates, int target,List<List<Integer>> ans,List<Integer> path ,int index)
+    {
+
+      if(target ==0)
+      {
+        ans.add(new ArrayList<>(path));
+        return;
+      }
+      if(target<0 || index== candidates.length)
+      {
+        return;
+      }
+
+      for(int i=index;i<candidates.length;i++)
+      {
+
+        if(i> index && candidates[i] == candidates[i-1])
         {
-             combination.add(new ArrayList<>(path));
-             return;
+            continue;
         }
-        if (target <0 ||  i == candidates.length)
+        if(candidates[i] > target)
         {
-            return;
+            break;
         }
-
-
-         for (int j= i; j < candidates.length; j++) {
-
-       if (j > i && candidates[j] == candidates[j- 1]){
-        continue;
-         }
-         if (candidates[j] > target)
-                break;
-        path.add(candidates[j]);
-        // SKIP
-        combsum(candidates, target-candidates[j],
-                combination, path, j + 1);
-        
-        // UNDO
-        path.remove(path.size() - 1);
-
-         }
-    }
+        path.add(candidates[i]);
+        backtrack(candidates, target-candidates[i] , ans, path,i+1);
+        path.remove(path.size()-1);
+      }
+    }    
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
-        List<List<Integer>> combination = new ArrayList<>();
+        List<List<Integer>> ans= new ArrayList<>();
         List<Integer> path = new ArrayList<>();
-       Arrays.sort(candidates);
-        combsum(candidates , target , combination, path,0);
-        return combination;
+          Arrays.sort(candidates);
+         backtrack(candidates, target , ans, path,0);
+         return ans;
     }
 }
