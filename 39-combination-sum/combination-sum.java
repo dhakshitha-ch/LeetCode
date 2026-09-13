41e195d194f5fcff7c7eddbb49a28f53f98c7d82
Dhakshitha <dhakshithachalla1@gmail.com>
2026-09-13 18:33:29 +0530

    Time: 3 ms (32.62%) | Memory: 46 MB (18.00%) - LeetSync

--- 39-combination-sum/combination-sum.java
+++ 39-combination-sum/combination-sum.java
@@ -1,38 +1,31 @@
 class Solution {
-
-    static void combsum(int[] candidates, int target,List<List<Integer>> combination , List<Integer> path,int i)
-    {   
-        if(target==0 )
+    static void backtrack(int[] candidates, int target, List<List<Integer>> ans,List<Integer> path, int index)
+    {
+        if(target==0)
         {
-             combination.add(new ArrayList<>(path));
-             return;
+            ans.add(new ArrayList<>(path));
+            return;
         }
-        if (target <0 ||  i == candidates.length)
+        if(target<0 || index== candidates.length)
         {
             return;
         }
-        
-
-        /// TAKE
-        path.add(candidates[i]);
-
-        // Take the same candidate again
-        combsum(candidates, target - candidates[i],
-                combination, path, i);
-
-        // UNDO
-        path.remove(path.size() - 1);
-
-        // SKIP
-        combsum(candidates, target,
-                combination, path, i + 1);
-
+        // path to add once 
+        path.add(candidates[index]);
+        // path to multiple 
+        backtrack(candidates, target-candidates[index],ans,path,index);
+        // undo 
+        path.remove(path.size()-1);
+        // dont add
+        backtrack(candidates,target,ans,path,index+1);
     }
-    public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<List<Integer>> combination = new ArrayList<>();
-        List<Integer> path = new ArrayList<>();
 
-        combsum(candidates , target , combination, path,0);
-        return combination;
+
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> ans=new ArrayList<>();
+        List<Integer> path = new ArrayList<>();
+        backtrack(candidates, target,ans,path, 0);
+        return ans;
+        
     }
 }
